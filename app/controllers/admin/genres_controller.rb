8732a7dbb606627d97e9a05cb73d939d9412c22a
EC2 Default User <ec2-user@ip-172-31-46-14.ec2.internal>
2022-03-23 10:15:07 +0000

    Add 管理者ジャンル登録、一覧ページ

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -4,11 +4,20 @@ class Admin::GenresController < ApplicationController
   end
 
   def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to admin_genres_path
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
