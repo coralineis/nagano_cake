b6c3c7b572c16e215ee9b7e1a7fe1caf2c7d7559
EC2 Default User <ec2-user@ip-172-31-46-14.ec2.internal>
2022-03-23 05:28:50 +0000

    Add customersコントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   scope module: :public do
+    devise_for :customers, skip: [:password], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+    }
+
     root to: 'homes#top'
     get 'about' => 'homes#about'
     resources :items, only: [:index, :show]
+    resources :customers, only: [:show, :edit, :update, :unsubscribe, :withdraw]
     resources :cart_items, only: [:create, :index, :update, :destroy, :destroy_all]
     resources :orders, only: [:new, :confirm, :complete, :complete, :create, :index, :show]
     resources :order_details, only: [:index, :edit, :create, :update, :destroy]
@@ -10,9 +16,6 @@ Rails.application.routes.draw do
   devise_for :admins, skip: [:registrations, :password], controllers: {
     sessions: "admin/sessions"
   }
-  devise_for :customers, skip: [:password], controllers: {
-  registrations: "public/registrations",
-  sessions: "public/sessions"
-  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
