4af3bee9e1a666f66495b18b72a13bcb7daf63a4
EC2 Default User <ec2-user@ip-172-31-46-14.ec2.internal>
2022-03-23 12:15:52 +0000

    Fix form_withのルートエラーのためURL指定

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,6 +1,7 @@
 class Admin::GenresController < ApplicationController
   def index
     @genre = Genre.new
+
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,8 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'homes#top'
     resources :items, except: [:destroy]
-    resources :genres, only: [:index, :create, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
     resources :orders, only: [:show, :update]
     resources :order_details, only: [:update]
   end
