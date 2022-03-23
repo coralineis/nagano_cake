540fea0d679c6f7aaecaa64fe5dcf7b4084404d7
EC2 Default User <ec2-user@ip-172-31-46-14.ec2.internal>
2022-03-23 05:11:55 +0000

    Add public/itemsコントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   scope module: :public do
     root to: 'homes#top'
-    get 'about' => 'homes#about', as: 'about'
+    get 'about' => 'homes#about'
+    resources :items, only: [:index, :show]
   end
   devise_for :admins, skip: [:registrations, :password], controllers: {
     sessions: "admin/sessions"
