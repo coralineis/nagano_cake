d0ff653232f8a9324399a13c77c89353a1770009
EC2 Default User <ec2-user@ip-172-31-46-14.ec2.internal>
2022-03-23 03:28:18 +0000

    Fix 顧客・管理者のルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :customers
+  devise_for :admins, skip: [:registrations, :password], controllers: {
+    sessions: "admin/sessions"
+  }
+  devise_for :customers, skip: [:password], controllers: {
+  registrations: "public/registrations",
+  sessions: "public/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
