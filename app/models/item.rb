8cd18ff65855865cf0f8c567c576b2687d3bfbb4
EC2 Default User <ec2-user@ip-172-31-46-14.ec2.internal>
2022-03-24 03:49:26 +0000

    Add itemテーブルにimage_idカラム追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  has_one_attached :image
+
 
   belongs_to :gerne
   has_many :order_details, dependent: :destroy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_23_094203) do
+ActiveRecord::Schema.define(version: 2022_03_24_034700) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -103,6 +103,7 @@ ActiveRecord::Schema.define(version: 2022_03_23_094203) do
     t.boolean "is_active"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_id"
   end
 
   create_table "order_details", force: :cascade do |t|
