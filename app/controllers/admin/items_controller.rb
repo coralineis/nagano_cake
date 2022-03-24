30f5173df752425ba716a2d1e705fb7d3fbd4dd8
EC2 Default User <ec2-user@ip-172-31-46-14.ec2.internal>
2022-03-24 05:56:39 +0000

    Update 商品登録画面、販売ステータス

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -24,6 +24,6 @@ class Admin::ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :introduction, :price, :image)
+    params.require(:item).permit(:name, :introduction, :price, :image, :is_active)
   end
 end
