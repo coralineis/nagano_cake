0fc9f1a61c190590948d9d32991cc95fa2e9e5d0
EC2 Default User <ec2-user@ip-172-31-46-14.ec2.internal>
2022-03-26 08:10:16 +0000

    Add admin/customerコントローラーにアクション追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -2,4 +2,16 @@ class Public::CustomersController < ApplicationController
   def show
     @customer = Customer.find(current_customer)
   end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def unsubscribe
+  end
+
+  def withdraw
+  end
 end
