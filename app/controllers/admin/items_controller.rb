09d3a2580bb1ff5130d88e0b575da84dd365b017
EC2 Default User <ec2-user@ip-172-31-46-14.ec2.internal>
2022-03-29 06:35:22 +0000

    Add admin 商品のジャンル選択機能

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -5,7 +5,7 @@ class Admin::ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    @item.save!
+    @item.save
     redirect_to admin_items_path
   end
 
@@ -27,6 +27,6 @@ class Admin::ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :introduction, :price, :image, :is_active)
+    params.require(:item).permit(:name, :introduction, :price, :image, :genre_id, :is_active)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   attachment :image
 
-  # belongs_to :genre
+  belongs_to :genre
   has_many :order_details, dependent: :destroy
   has_many :cart_items, dependent: :destroy
   has_many :customers, through: :cart_items
