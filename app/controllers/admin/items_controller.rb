6aa0a9c79708c971023240d10beac0e44d64c8ef
EC2 Default User <ec2-user@ip-172-31-46-14.ec2.internal>
2022-03-29 06:51:24 +0000

    Add 商品編集ページ

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -22,6 +22,9 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admin_item_path(@item.id)
   end
 
   private
