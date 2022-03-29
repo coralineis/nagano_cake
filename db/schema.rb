74c9ee1b11fa48abdde5083ee0666744ec25968d
EC2 Default User <ec2-user@ip-172-31-46-14.ec2.internal>
2022-03-29 05:50:20 +0000

    Fix Genresテーブル作り直し

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_23_035027) do
+ActiveRecord::Schema.define(version: 2022_03_29_054850) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -59,6 +59,12 @@ ActiveRecord::Schema.define(version: 2022_03_23_035027) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "order_details", force: :cascade do |t|
