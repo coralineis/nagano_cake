1f7d1cdcd1ff9214a4e1a46806485634499abb0f
EC2 Default User <ec2-user@ip-172-31-46-14.ec2.internal>
2022-03-29 05:55:20 +0000

    Fix itemsテーブル作り直し

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_29_054850) do
+ActiveRecord::Schema.define(version: 2022_03_29_055045) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "customer_id", null: false
@@ -63,6 +63,17 @@ ActiveRecord::Schema.define(version: 2022_03_29_054850) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.integer "genre_id", null: false
+    t.string "name", null: false
+    t.text "introduction", null: false
+    t.integer "price", null: false
+    t.boolean "is_active", default: true, null: false
+    t.string "image_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
