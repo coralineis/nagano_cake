8245b8464ff1afa698d35119c8e963b596fe7b09
EC2 Default User <ec2-user@ip-172-31-46-14.ec2.internal>
2022-03-30 03:19:46 +0000

    Add admin会員詳細ページ

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,10 +3,18 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :addresses, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_many :cart_items, dependent: :destroy
   has_many :items, through: :cart_items
+
+  def full_name
+    self.last_name + self.first_name
+  end
+
+  def full_name_kana
+    self.last_name_kana + self.first_name_kana
+  end
 
 end
