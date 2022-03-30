6c9e562e7335fe610460271311b86caea78d4312
EC2 Default User <ec2-user@ip-172-31-46-14.ec2.internal>
2022-03-30 04:13:15 +0000

    Fix admin会員情報編集の変更保存ボタンエラー

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -12,7 +12,7 @@ class Admin::CustomersController < ApplicationController
   end
 
   def update
-    @customer = Cusomer.find(params[:id])
+    @customer = Customer.find(params[:id])
     @customer.update(customer_params)
     redirect_to admin_customer_path(@customer.id)
   end
