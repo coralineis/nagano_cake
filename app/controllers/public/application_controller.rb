ac2cea4549e297fff21e00b9df7ab57cb6f47428
EC2 Default User <ec2-user@ip-172-31-46-14.ec2.internal>
2022-03-30 05:55:26 +0000

    Fix applicationコントローラー

--- app/controllers/public/application_controller.rb
+++ app/controllers/public/application_controller.rb
@@ -1,22 +1,2 @@
 class Public::ApplicationController < ApplicationController
-  before_action :authenticate_customer!, except: [:top, :about]
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def after_sign_up_path_for(resource)
-    my_page_path
-  end
-
-  def after_sign_in_path_for(resource)
-    root_path
-  end
-
-  def after_sign_out_path_for(resource)
-    root_path
-  end
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number])
-  end
 end
