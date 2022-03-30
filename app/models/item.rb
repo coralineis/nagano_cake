702d8f959a36978cef6708a51347a54ec76e1353
EC2 Default User <ec2-user@ip-172-31-46-14.ec2.internal>
2022-03-30 07:01:44 +0000

    Fix 商品詳細値段表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,8 @@ class Item < ApplicationRecord
   has_many :order_details, dependent: :destroy
   has_many :cart_items, dependent: :destroy
   has_many :customers, through: :cart_items
+
+  def add_tax_price
+    (self.price * 1.08).round
+  end
 end
