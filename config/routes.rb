abaeb711643ae7ee54992a021104984738e0c733
EC2 Default User <ec2-user@ip-172-31-46-14.ec2.internal>
2022-03-31 09:13:06 +0000

    Fix ルーティング間違い修正

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
     patch 'customers/withdraw' => 'customers#withdraw', as: 'withdraw'
     resources :cart_items, only: [:create, :index, :update, :destroy, :destroy_all]
     resources :orders, only: [:new, :confirm, :complete, :complete, :create, :index, :show]
-    resources :order_details, only: [:index, :edit, :create, :update, :destroy]
+    resources :addresses, except: [:new, :show]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
