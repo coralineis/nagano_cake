b8795a936c550cd9fd7fc88b0de60891269a50a8
EC2 Default User <tavylrioly@gmail.com>
2022-04-01 08:15:44 +0000

    Update 商品詳細ページ

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,11 @@
 class Public::CartItemsController < ApplicationController
+  def create
+    @item = Item.new(cart_item_params)
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
+  end
 end
