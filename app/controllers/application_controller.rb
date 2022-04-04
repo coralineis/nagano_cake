126bcef319272ee151de687b911889f9ff4ce775
EC2 Default User <tavylrioly@gmail.com>
2022-04-04 05:15:11 +0000

    Fix ログアウトできない

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,9 +18,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(resource)
     case resource
-    when Customer
+    when :customer
       root_path
-    when Admin
+    when :admin
       new_admin_session_path
     end
   end
