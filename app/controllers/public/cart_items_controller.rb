37e2d3c7d2db14c734d08f04f407d64261098a9c
EC2 Default User <tavylrioly@gmail.com>
2022-04-04 08:43:54 +0000

    Update カート内商品一覧

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -8,17 +8,21 @@ class Public::CartItemsController < ApplicationController
 
   def index
     @cart_items = current_customer.cart_items
-    @total_payment
+    @total_payment = 0
   end
 
   def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to cart_items_path
   end
 
   def destroy
   end
 
   def destroy_all
-    @item = CartItem.destroy_all
+    @cart_item = CartItem.find(params[:id])
+    CartItem.destroy_all
     redirect_to cart_items_path
   end
 
