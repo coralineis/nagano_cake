1b5702099adf045e5570e021421ec608174664d2
EC2 Default User <tavylrioly@gmail.com>
2022-04-05 06:56:22 +0000

    Update 配送先一覧画面

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -6,6 +6,7 @@ class Public::AddressesController < ApplicationController
 
   def create
     @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
     @address.save
     redirect_to addresses_path
   end
