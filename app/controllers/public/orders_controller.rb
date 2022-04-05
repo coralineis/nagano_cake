a2613b6c6d0eb246ef7f60e24de9a952772eba9b
EC2 Default User <tavylrioly@gmail.com>
2022-04-05 08:23:18 +0000

    Fix 注文情報入力画面レイアウト調整

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,4 +1,31 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
+  end
+
+  def confirm
+    @order = Order.new(order_params)
+    @order = Address.find(params[:order][:address_id])
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.full_name
+  end
+
+  def complete
+  end
+
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
   end
 end
