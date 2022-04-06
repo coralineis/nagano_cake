a52dadddb24da2127ec7a8f493b32426ac47b765
EC2 Default User <tavylrioly@gmail.com>
2022-04-06 06:03:05 +0000

    Fix 配送先編集画面

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -17,6 +17,8 @@ class Public::AddressesController < ApplicationController
 
   def update
     @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to addresses_path
   end
 
   def destroy
