f0361bd82835403ed29803584fd73ba3694c29dc
EC2 Default User <tavylrioly@gmail.com>
2022-04-06 07:13:09 +0000

    Fix 注文確認画面 住所選択

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,25 +5,29 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
-    if params[:order][:select_address] == 0
+    if params[:order][:select_address] == '0'
       @order.postal_code = current_customer.postal_code
       @order.address = current_customer.address
       @order.name = current_customer.full_name
-    elsif params[:order][:select_address] == 1
+    elsif params[:order][:select_address] == '1'
       @order = Address.find(params[:order][:address_id])
       @order.postal_code = @address.postal_code
       @order.address = @address.address
       @order.name = @address.name
     else
-
+      @order.postal_code = params[:order][:postal_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
     end
     @orders = current_customer.cart_items
-    @total_payment = 0
+    @total_payment = @orders.inject(0) {|sum, item| sum + item.subtotal }
   end
 
   def create
     @order = Order.new(order_params)
     @order.save
+
+    current_customer.cart_items.destroy_all
     redirect_to complete_path
   end
 
