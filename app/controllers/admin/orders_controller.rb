14fff9de757599fa513551801f083d18f5770a74
EC2 Default User <tavylrioly@gmail.com>
2022-04-07 09:18:09 +0000

    Update admin注文履歴詳細

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,6 +1,12 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
-    @customer = Customer.find(params[:id])
+    @order_details = OrderDetail.all
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.update
+    redirect_to admin_order_path(@order.id)
   end
 end
