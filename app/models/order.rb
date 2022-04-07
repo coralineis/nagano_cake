de004f8f5f99fe3ec75ad0965a552fafa963bacc
EC2 Default User <tavylrioly@gmail.com>
2022-04-07 01:09:26 +0000

    Add 製作ステータスenum

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,6 @@ class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
   has_many :items, through: :order_details
   enum payment_method: { credit_card: 0, transfer: 1 }
-  enum status: { pre_order: 0, order_placed: 1, making_items: 2, prepare_for_shipping: 3, shipped: 4 }
+  enum status: { pre_order: 0, order_placed: 1, making_products: 2, prepare_for_shipping: 3, shipped: 4 }
 
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,6 +1,6 @@
 class OrderDetail < ApplicationRecord
   belongs_to :item
   belongs_to :order
-  
-  enum making_status: {}
+
+  enum making_status: { discontinued: 0, preparing: 1, processing: 2, complete: 3 }
 end
