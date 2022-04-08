b60abbd82ae6cc5fdc4624e8a37068db27dee959
EC2 Default User <tavylrioly@gmail.com>
2022-04-08 04:31:27 +0000

    Add 受注ステータス

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,7 +6,12 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    @order.update
+    @order.update(order_params)
     redirect_to admin_order_path(@order.id)
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:status, :making_status)
   end
 end
